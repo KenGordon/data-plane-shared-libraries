a777dddf3e82e214eb278a5dcf5ab62c0c2da38a
Dominic Ayre <dominicayre@microsoft.com>
2025-04-07 16:51:51 +0100

    Get ACL scoped tokens (#50)
    * Get tokens for ACL
    
    * Satisfy precommit
    
    * Fix test
    
    * Add comment about parameterization

--- src/cpio/client_providers/auth_token_provider/azure/azure_auth_token_provider.cc
+++ src/cpio/client_providers/auth_token_provider/azure/azure_auth_token_provider.cc
@@ -61,8 +61,11 @@ constexpr char kMetadataHeaderValue[] = "true";
 // https://learn.microsoft.com/en-us/azure/container-instances/container-instances-managed-identity
 constexpr char kDefaultGetTokenUrl[] =
     "http://169.254.169.254/metadata/identity/oauth2/token";
+// This could be parameterized as an env variable, but for now keeping this
+// hardcoded to save time
 constexpr char kGetTokenQuery[] =
-    "?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F";
+    "?api-version=2018-02-01&resource=https%3A%2F%2Fconfidential-ledger.azure."
+    "com%2F";
 constexpr char kGetTokenUrlEnvVar[] = "AZURE_BA_PARAM_GET_TOKEN_URL";
 constexpr char kJsonAccessTokenKey[] = "access_token";
 constexpr char kJsonTokenExpiryKey[] = "expires_in";
